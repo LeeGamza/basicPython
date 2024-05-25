6ac2254c04b8593d1d2432d9b5c64af6ff412bcb
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-25 21:31:07 +0900

    연산자

---
+++ practice4.py
@@ -0,0 +1,36 @@
+# 연산자
+
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) # 2의 3제곱
+print(5 % 3) # 5/3을 한 나머지
+print(10 % 3)
+print(5//3) # 5/3을 한 몫
+print(10//3)
+
+# 비교연산자
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 3)
+print(4 == 2)
+print(3 + 4 == 7)
+
+print(1 != 3)
+print(not(1 != 3))
+
+# and 조건
+print((3 >0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+# or 조건
+print((3 > 0) or (3 > 5))
+print((3 > 0) | (3 > 5))
+
+print(5 > 4 > 3)
+print(5 > 4 > 7)
