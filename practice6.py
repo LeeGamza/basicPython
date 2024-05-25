e68a2cb735dc6cb3e42b441215944e39beb559ad
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-26 01:32:39 +0900

    숫자 처리 함수

---
+++ practice6.py
@@ -0,0 +1,13 @@
+# 숫자 처리 함수
+
+print(abs(-5)) # 절댓값 반환 함수(abs) 
+print(pow(4, 2)) # 거듭제곱 반환 함수 ex) pow(4, 2) = 4^2
+print(max(5, 12)) # 최댓값 반환 함수
+print(min(5, 12)) # 최솟값 반환 함수
+print(round(3.14)) # 반올림 반환 함수
+print(round(4.99))
+
+from math import *
+print(floor(4.99)) # 내림 함수
+print(ceil(3.14)) # 올림 함수
+print(sqrt(16)) # 제곱근 반환함수 ex) 16의 제곱근 = 4
