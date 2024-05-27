03024670aa10711c2b0032b898feab0cb251b4da
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-27 14:23:35 +0900

    자료구조의 변경

--- practice16.py
+++ practice16.py
@@ -3,6 +3,7 @@
 
 my_set = {1,2,3,3,3}
 print(my_set)
+# 1,2,3
 
 java = {"유재석", "김태호", "양세형"}
 python = set(["유재석","박명수"])
@@ -10,19 +11,24 @@ python = set(["유재석","박명수"])
 # 교집합 (java 와 python을 모두 할 수 있는 개발자)
 print(java & python)
 print(java.intersection(python))
+# 유재석
 
 # 합집합 (java 도 할 수 있거나, python 할 수 있는 개발자)
 print(java | python)
 print(java.union(python))
+# 유재석 김태호 양세형 박명수
 
 # 차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
 print(java - python)
 print(java.difference(python))
+# 김태호, 양세형
 
 # python 할 줄 아는 사람이 늘어남
 python.add("김태호")
 print(python)
+# 유재석 박명수 김태호
 
 # java를 잊었어요
 java.remove("김태호")
 print(java)
+# 유재석 양세형
