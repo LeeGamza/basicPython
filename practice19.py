ca12a4acebf29d5a4fba55d67abe44a96b88c135
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-28 17:17:58 +0900

    for(반복문)

--- practice19.py
+++ practice19.py
@@ -1,2 +1,16 @@
 # for(반복문)
 
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+# for waiting_num in [0,1,2,3,4]:
+#     print(f"대기번호 : {waiting_num}")
+
+# for waiting_num in range(1, 6):
+#     print(f"대기번호 : {waiting_num}")
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
