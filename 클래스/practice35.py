fdfed5fe49c8f714b08f5f4ff487daa5e9f8cf88
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-05-29 10:08:13 +0900

    init

---
+++ 클래스/practice35.py
@@ -0,0 +1,12 @@
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print(f"{self.name} 유닛이 생성 되었습니다.")
+        print(f"체력 {self.hp}, 공격력 {self.damage}")
+
+marine1 = Unit("마린", 40, 5) # instance
+marine2 = Unit("마린", 40, 5) # instance
+tank = Unit("탱크", 150, 35) # instance
+marine3 = Unit("마린",40)
